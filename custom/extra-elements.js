6563f7aa0ac47af7f18eee89dca612c158df918f
Jody Hamiliton <jody@zivtech.com>
2014-10-24 17:57:06 -0400

    adding to github

---
+++ custom/extra-elements.js
@@ -0,0 +1,14 @@
+   $(document).bind('deck.change', function(event, from, to) {
+   	  if (to == 3) {
+   	  	// Add menus
+   	  }
+   	  if (to == 4) {
+   	  	// add path
+   	  }
+   	  if (to == 5) {
+   	  	// add breadcrumb
+   	  }
+   	  if (to > 5) {
+   	  	// ensure menus, paths and breadcrumbs are added.
+   	  }
+    });
